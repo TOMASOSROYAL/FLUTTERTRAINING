b13c583c46881fc1f3807dab32242f448d1b7c35
TOMASOSROYAL <46319831+TOMASOSROYAL@users.noreply.github.com>
2024-12-02 08:28:36 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       title: 'Krisi Homo',
       debugShowCheckedModeBanner: false,
-=======
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false, 
-<<<<<<< Updated upstream
-=======
->>>>>>> 75df162439aa2d4c9415445f2af0c9c45cf0b4e0
->>>>>>> Stashed changes
       theme: ThemeData(
-        primarySwatch: Colors.pink ),
+        primarySwatch: Colors.blue,
+      ),
       home: const MyHomePage(title: 'krisi ohel batahat'),
     );
   }
@@ -64,62 +57,23 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-<<<<<<< Updated upstream
-              'krisi ohel batahat', 
-            ),
-            Text(
-              '$_counter', 
-=======
-<<<<<<< HEAD
               'krisi ohel batahat',
             ),
             Text(
               '$_counter',
-=======
-              'krisi ohel batahat', 
-            ),
-            Text(
-              '$_counter', 
->>>>>>> 75df162439aa2d4c9415445f2af0c9c45cf0b4e0
->>>>>>> Stashed changes
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: Padding(
-<<<<<<< Updated upstream
-        padding: const EdgeInsets.only(right: 16.0), 
-        child: Row(
-=======
-<<<<<<< HEAD
         padding: const EdgeInsets.only(right: 16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             MinusButton(onPressed: _decrementCounter), // Use MinusButton
-            const SizedBox(width: 15),
+            const SizedBox(width: 10),
             PlusButton(onPressed: _incrementCounter), // Use PlusButton
-=======
-        padding: const EdgeInsets.only(right: 16.0), 
-        child: Row(
->>>>>>> Stashed changes
-          mainAxisAlignment: MainAxisAlignment.end, 
-          children: [
-            FloatingActionButton(
-              heroTag: 'decrement', 
-              onPressed: _decrementCounter, 
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            ),
-            const SizedBox(width: 10), 
-            FloatingActionButton(
-              heroTag: 'increment', 
-              onPressed: _incrementCounter, 
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
->>>>>>> 75df162439aa2d4c9415445f2af0c9c45cf0b4e0
           ],
         ),
       ),
